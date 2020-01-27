c74ac0d5fd6a5c1f3617a6a47409a6088a73d0e5
Nicolas Bancel <bancelnicolas@gmail.com>
2020-01-27 15:57:56 +0100

    first page

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -8,14 +8,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class WelcomeController extends AbstractController
 {
+
     /**
      * @Route("/hello", name="hello")
      */
     public function hello()
     {
         $name = 'Nicolas';
-
-        dump($name);
 
         return $this->render('welcome/hello.html.twig', [
             'name' => $name,
@@ -24,6 +23,23 @@ class WelcomeController extends AbstractController
         // return new Response(
         //     '<body>Salut '.$name.'</body>'
         // );
+
+    }
+
+    /**
+     * @route(
+     * "/hello/{slug}",
+     * name="hello",
+     * requirements={"slug"="\w{3,8}"}
+     * )
+     */
+    public function show($slug = 'Nicola')
+    {
+        $name = $slug;
+
+        return $this->render('welcome/hello.html.twig', [
+            'name' => $name,
+        ]);
 
     }
 
